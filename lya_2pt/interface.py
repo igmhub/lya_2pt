9af02295ea6bd09f84fb25abbf7226087b797342
Andrei Cuceu <andreicuceu@gmail.com>
2023-01-12 11:03:09 +0100

    Add template interface

---
+++ lya_2pt/interface.py
@@ -0,0 +1,11 @@
+# import mpi4py
+
+
+class Interface:
+    """Interface for lya_2pt package
+    Read ini files
+    Handle parallezation
+        - Read data
+        - Call individual compute functions
+    Write outputs
+    """
