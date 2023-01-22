24bb8d3451b1fb44f18f0f7eba97b91496c5a679
Andrei Cuceu <andreicuceu@gmail.com>
2023-01-22 13:23:15 +0000

    Healpix reader now working with hdu read

--- lya_2pt/forest_healpix_reader.py
+++ lya_2pt/forest_healpix_reader.py
@@ -1,20 +1,29 @@
 """This file defines the class ForestReader used to read the data"""
 
+import fitsio
 import numpy as np
+from numba import njit
 from healpy import query_disc
+from multiprocessing import Pool
 
-from lya_2pt.utils import parse_config
+from lya_2pt.utils import parse_config, compute_ang_max
 from lya_2pt.tracer import Tracer
+from lya_2pt.absorbers import ABSORBER_IGM
 
-from picca.delta_extraction.utils import ABSORBER_IGM
-
-accepted_options = ["absorption line", "num processors",  "order"]
+accepted_options = ["absorption_line", "project_deltas", "num_processors", "order",
+                    "rebin", "z_min", "z_max", "nside"]
 
 defaults = {
-    "absorption line": "LYA",
-    "num processors": 1,
+    "absorption_line": "LYA",
+    "num_processors": 1,
+    "project_deltas" : True,
     "order": 1,
+    "rebin": 1,
+    "z_min": 0,
+    "z_max": 10,
+    "nside": 16
 }
+
 
 class ForestHealpixReader:
     """Class to read the data of a forest-like tracer
@@ -41,7 +50,7 @@ class ForestHealpixReader:
     tracers: array of Tracer
 
     """
-    def __init__(self, config, file):
+    def __init__(self, config, file, cosmo):
         """Initialize class instance
 
         Arguments
@@ -53,82 +62,65 @@ class ForestHealpixReader:
         Name of the file to read
         """
         # locate files
-        reader_config = parse_config(config["reader"], defaults, accepted_options)
-
-        # intialize cosmology
-        cosmo = Cosmology(config["cosmology"])
+        reader_config = parse_config(config, defaults, accepted_options)
 
         # figure out format and blinding
         self.tracers = None
-        hdu = fitsio.FITS(input_file)
+        hdul = fitsio.FITS(file)
+
+        absorption_line = reader_config.get("absorption_line")
         # image format
-        if "METADATA" in hdu:
-            self.tracers = read_from_image(
-                input_file,
-                reader_config.get("absorption line"))
-            self.blinding = hdu["METADATA"].read_header()["BLINDING"]
+        if "METADATA" in hdul:
+            self.tracers, self.wave_solution = read_from_image(hdul, absorption_line)
+            self.blinding = hdul["METADATA"].read_header()["BLINDING"]
         # HDU per forest
         else:
-            self.tracers = read_from_image(input_file)
-            self.blinding = hdu[1].read_header()["BLINDING"]
+            self.tracers, self.wave_solution = read_from_hdu(hdul, absorption_line)
+            self.blinding = hdul[1].read_header()["BLINDING"]
 
         # rebin
         if config.getint("rebin") > 1:
-            if reader_config.getint("num processors") > 1:
-                arguments = [
-                    (tracer.log_lambda, tracer.flux, rebin_factor)
-                    for tracer in self.tracers
-                ]
-                pool = Pool(processes=reader_config.getint("num processors"))
-                results = pool.starmap(rebin, arguments)
-                pool.close()
+            if reader_config.getint("num_processors") > 1:
+                arguments = [(tracer.log_lambda, tracer.deltas, tracer.weights,
+                              config.getint("rebin"), self.wave_solution)
+                             for tracer in self.tracers]
+                with Pool(processes=reader_config.getint("num_processors")) as pool:
+                    results = pool.starmap(rebin, arguments)
             else:
-                results = [
-                    rebin(tracer.log_lambda, tracer.flux, rebin_factor)
-                    for tracer in self.tracers
-                ]
-            for tracer, (log_lambda, deltas, weights) in zip(tracers, results):
+                results = [rebin(tracer.log_lambda, tracer.deltas, tracer.weights,
+                                 config.getint("rebin"), self.wave_solution)
+                           for tracer in self.tracers]
+
+            for tracer, (log_lambda, deltas, weights) in zip(self.tracers, results):
                 tracer.log_lambda = log_lambda
                 tracer.deltas = deltas
                 tracer.weights = weights
 
-
         # project
-        if config.getint("project deltas"):
-            if reader_config.getint("num processors") > 1:
-                arguments = [
-                    (tracer.log_lambda, tracer.deltas, tracer.weights,
-                     reader_config.getint("order"))
-                    for tracer in self.tracers
-                ]
-                pool = Pool(processes=reader_config.getint("num processors"))
-                results = pool.starmap(project_deltas, arguments)
-                pool.close()
+        if config.getboolean("project_deltas"):
+            if reader_config.getint("num_processors") > 1:
+                arguments = [(tracer.log_lambda, tracer.deltas, tracer.weights,
+                              reader_config.getint("order"))
+                             for tracer in self.tracers]
+                with Pool(processes=reader_config.getint("num_processors")) as pool:
+                    results = pool.starmap(project_deltas, arguments)
             else:
-                results = [
-                    project_deltas(
-                        tracer.log_lambda,
-                        tracer.deltas,
-                        tracer.weights,
-                        reader_config.getint("order"),
-                    )
-                    for tracer in tracers
-                ]
+                results = [project_deltas(tracer.log_lambda, tracer.deltas, tracer.weights,
+                                          reader_config.getint("order"),)
+                           for tracer in self.tracers]
 
-            for tracer, (log_lambda, deltas, weights) in zip(tracers, results):
+            for tracer, deltas in zip(self.tracers, results):
                 tracer.deltas = deltas
 
         # add distances
-        [tracer.compute_comoving_distances(cosmo) for tracer in tracers]
-
-        self.z_min = config["cuts"].getfloat('z_min', 0.)
-        self.z_max = config["cuts"].getfloat('z_max', 10.)
+        for tracer in self.tracers:
+            tracer.compute_comoving_distances(cosmo)
 
         # We need to figure out somewhere if this is an auto-correlation or not
         # and initialize this flag (i.e. if data_set_1 = data_set_2)
         self.auto_flag = True
 
-    def find_healpix_neighbours(self):
+    def find_healpix_neighbours(self, nside, ang_max):
         """Find the healpix neighbours
 
         Return
@@ -136,17 +128,18 @@ class ForestHealpixReader:
         healpix_ids: array of int
         The healpix id of the neighbouring healpixes
         """
+        assert self.tracers is not None, "Forest reader failure"
+
         # TODO We need to initialize nside (read from config) and ang_max (compute using cosmo)
         neighbour_ids = set()
         for tracer in self.tracers:
-            tracer_neighbour_ids = query_disc(self.nside, [tracer.x_cart, tracer.y_cart, tracer.z_cart],
-                                              self.ang_max, inclusive=True)
+            tracer_neighbour_ids = query_disc(nside, [tracer.x_cart, tracer.y_cart, tracer.z_cart],
+                                              ang_max, inclusive=True)
             neighbour_ids = neighbour_ids.union(set(tracer_neighbour_ids))
 
-        return np.array(neighbour_ids)
-            
+        return np.array(list(neighbour_ids))
 
-    def find_neighbours(self, other_tracers):
+    def find_neighbours(self, other_tracers, z_min, z_max):
         """
 
         Arguments
@@ -154,17 +147,18 @@ class ForestHealpixReader:
         other_tracers: array of Tracer
         Other tracers
         """
+        assert self.tracers is not None, "Forest reader failure"
+
         for tracer1 in self.tracers:
             neighbour_mask = np.full(other_tracers.shape, False)
 
             for i, tracer2 in enumerate(other_tracers):
-                if tracer1.check_if_neighbour(tracer2, self.auto_flag,
-                                              self.z_min, self.z_max):
+                if tracer1.check_if_neighbour(tracer2, self.auto_flag, z_min, z_max):
                     neighbour_mask[i] = True
 
             tracer1.add_neighbours(neighbour_mask)
 
-def read_from_image(input_file, cosmo, absorption_line):
+def read_from_image(hdul, absorption_line):
     """Read data with image format
 
     Arguments
@@ -184,36 +178,38 @@ def read_from_image(input_file, cosmo, absorption_line):
     tracers: array of Tracer
     The loaded tracers
     """
-    hdul = fitsiio.FITS(input_file)
+    # hdul = fitsio.FITS(input_file)
 
-    header = hdul["METADATA"].read_header()
-    num_forests = hdul["METADATA"].get_nrows()
+    # header = hdul["METADATA"].read_header()
+    # num_forests = hdul["METADATA"].get_nrows()
 
     los_id_array = hdul["METADATA"]["LOS_ID"][:]
     ra_array = hdul["METADATA"]["RA"][:]
     dec_array = hdul["METADATA"]["DEC"][:]
 
-    deltas_array = hdul["DELTA_BLIND"].read().astype(float)
+    deltas_array = hdul["DELTA"].read().astype(float)
     weights_array = hdul["WEIGHT"].read().astype(float)
+    wave_solution = None
     if "LOGLAM" in hdul:
         log_lambda = hdul["LOGLAM"][:].astype(float)
         z = 10**log_lambda/ABSORBER_IGM.get(absorption_line) - 1.0
+        wave_solution = 'log'
     elif "LAMBDA" in hdul:
         lambda_ = hdul["LAMBDA"][:].astype(float)
         log_lambda = np.log10(lambda_)
         z = lambda_/ABSORBER_IGM.get(absorption_line) - 1.0
+        wave_solution = 'lin'
     else:
         raise KeyError("Did not find LOGLAM or LAMBDA in delta file")
 
-    tracers = np.array([
-        Tracer(los_id, ra, dec, deltas_array[index], weights_array[index],
-               log_lambda, z, cosmo)
-        for index, (los_id, ra, dec) in enumerate(zip(los_id_array, ra_array, dec_array))
-    ])
+    tracers = np.array([Tracer(los_id, ra, dec, deltas_array[index],
+                               weights_array[index], log_lambda, z)
+                        for index, (los_id, ra, dec)
+                        in enumerate(zip(los_id_array, ra_array, dec_array))])
 
-    return tracers
+    return tracers, wave_solution
 
-def read_from_hdu(input_file, cosmo, absorption_line):
+def read_from_hdu(hdul, absorption_line):
     """Read data with an HDU per forest
 
     Arguments
@@ -233,30 +229,35 @@ def read_from_hdu(input_file, cosmo, absorption_line):
     tracers: array of Tracer
     The loaded tracers
     """
-    hdul = fitsio.FITS(input_file)
+    # hdul = fitsio.FITS(input_file)
 
     tracers = []
     for hdu in hdul[1:]:
         header = hdu.read_header()
 
-        los_id = header["LOS_ID"][:]
+        los_id = header["LOS_ID"]
         ra = header['RA']
         dec = header['DEC']
 
-        delta = hdu["DELTA_BLIND"][:].astype(float)
+        delta = hdu["DELTA"][:].astype(float)
+        weights = hdu["WEIGHT"][:].astype(float)
+        wave_solution = None
         if 'LOGLAM' in hdu.get_colnames():
             log_lambda = hdu['LOGLAM'][:].astype(float)
             z = 10**log_lambda/ABSORBER_IGM.get(absorption_line) - 1.0
+            wave_solution = 'log'
         elif 'LAMBDA' in hdu.get_colnames():
             lambda_ = hdu['LAMBDA'][:].astype(float)
             log_lambda = np.log10(lambda_)
             z = lambda_/ABSORBER_IGM.get(absorption_line) - 1.0
+            wave_solution = 'lin'
         else:
             raise KeyError("Did not find LOGLAM or LAMBDA in delta file")
 
-        tracers.append(Tracer(los_id, ra, dec, deltas, weights, log_lambda, z, cosmo))
+        tracers.append(Tracer(los_id, ra, dec, delta, weights, log_lambda, z))
 
-    return np.arrays(tracers)
+    return np.array(tracers), wave_solution
+
 
 @njit()
 def rebin(log_lambda, deltas, weights, rebin_factor, wave_solution):
@@ -291,7 +292,7 @@ def rebin(log_lambda, deltas, weights, rebin_factor, wave_solution):
     rebin_weight: array of float
     The rebinned array for the weights
     """
-    # find neew grid
+    # find new grid
     if wave_solution == "lin":
         lambda_ = 10**np.array(log_lambda)
         rebin_lambda = np.average(lambda_.reshape(-1, rebin_factor), axis=1)
@@ -307,6 +308,7 @@ def rebin(log_lambda, deltas, weights, rebin_factor, wave_solution):
     rebin_weights = np.bincount(bins, weights=weights, minlength=binned_arr_size)
 
     return rebin_log_lambda, rebin_deltas, rebin_weights
+
 
 @njit()
 def project_deltas(log_lambda, deltas, weights, order):
