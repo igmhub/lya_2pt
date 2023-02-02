25178f6d42d45e91f56a3b52b886775cb2559a54
Andrei Cuceu <andreicuceu@gmail.com>
2023-02-02 18:50:51 +0000

    Add script

--- bin/cf.py
+++ bin/cf.py
@@ -1,2 +1,18 @@
+import argparse
+from configparser import ConfigParser
+
+from lya_2pt.interface import Interface
+
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     description=('Compute auto-correlation function'))
+
+    parser.add_argument('-i', '--config', type=str, default=None,
+                        help=('Path to config file'))
+
+    args = parser.parse_args()
+
+    config = ConfigParser()
+    config.read(args.config)
+
+    Interface(config)
