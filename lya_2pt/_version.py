fa159579e8628687dbc1600756b29dcc3d40a335
Andrei Cuceu <andreicuceu@gmail.com>
2023-03-12 16:20:57 -0400

    Change version location

--- lya_2pt/_version.py
+++
@@ -1,3 +0,0 @@
-__author__ = """Andrei Cuceu, Ignasi Pérez-Ràfols et al"""
-__email__ = 'andreicuceu@gmail.com'
-__version__ = '0.1'
--- setup.py
+++ setup.py
@@ -6,14 +6,16 @@ import git
 from setuptools import find_namespace_packages, setup
 from pathlib import Path
 
+__author__ = """Andrei Cuceu, Ignasi Pérez-Ràfols et al"""
+__email__ = 'andreicuceu@gmail.com'
+__version__ = '0.1'
+
 scripts = sorted(glob.glob('bin/*'))
 
 description = (f"Package for computing 3D correlations from Lya forest and associated tracers\n"
                f"commit hash: {git.Repo('.').head.object.hexsha}")
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-
-exec(open('lya_2pt/_version.py').read())
 
 setup(name="lya_2pt",
       version=__version__,
