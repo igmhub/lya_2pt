da20b1d320548d95b642ff553cb00b8ed574a684
Naim Goksel Karacayli <ngokselk@gmail.com>
2023-08-16 14:25:12 -0400

    build_xi1d arguments and wavelength grid

--- lya_2pt/optimal_estimator.py
+++ lya_2pt/optimal_estimator.py
@@ -6,27 +6,32 @@ from scipy.interpolate import RegularGridInterpolator as RGI
 import lya_2pt.global_data as globals
 
 
-def fiducial_Pk(kv, z, A=0.066, k0=0.009, z0=3.0, n=-2.685, alpha=-0.22, k1=0.053, B=3.59, beta=-0.18):
+def fiducial_Pk(
+        kv, z, k0=0.009, z0=3.0, A=0.066, n=-2.685, alpha=-0.22, k1=0.053, B=3.59, beta=-0.18
+):
     log_k_k0 = np.log(kv / k0 + 1e-10)
     pk = A * np.power(kv / k0, 2 + n + alpha * log_k_k0) / (1 + (kv / k1)**2)
     z_evol = np.power((1 + z) / (1 + z0), B + beta * log_k_k0)
-    return pk * z_evol * np.pi / k0
+    return pk * z_evol * np.pi / k0  # km / s
 
 
-def fiducial_Pk_angstrom(kA, z, A=0.066, k0=0.009, z0=3.0, n=-2.685, alpha=-0.22, k1=0.053, B=3.59, beta=-0.18):
-    kv = kA * 1215.67 * (1 + z) / (speed_of_light / 1000) 
-    return fiducial_Pk(kv, z, A, k0, z0, n, alpha, k1, B, beta) * 1215.67 * (1 + z) / (speed_of_light / 1000)
+def fiducial_Pk_angstrom(
+        kA, z, k0=0.009, z0=3.0, A=0.066, n=-2.685, alpha=-0.22, k1=0.053, B=3.59, beta=-0.18
+):
+    c_kms = speed_of_light / 1000
+    A2kms = 1215.67 * (1 + z) / c_kms  # A / (km/s)
+    kv = kA * A2kms
+    return fiducial_Pk(kv, z, A, k0, z0, n, alpha, k1, B, beta) * A2kms
 
 
 def window_squared_angstrom(k, delta_lambda=2.4, R=0.8):
-    return (np.exp(-k**2 * R**2 / 2) * np.sinc(k * delta_lambda / 2 / np.pi))**2
+    return np.exp(-k**2 * R**2) * np.sinc(k * delta_lambda / 2 / np.pi)**2
 
 
-def build_xi1d():
-    z = np.linspace(1.8, 4, 50)
+def build_xi1d(z1=1.8, z2=4., nz=50, lambda_max=2048):
+    z = np.linspace(z1, z2, nz)
 
-    dlambda = 0.01
-    r = dlambda * (np.arange(2**20) - 2**19)
+    r, dlambda = np.linspace(-lambda_max, lambda_max, int(10 * lambda_max) + 1, retstep=True)
     k = np.fft.rfftfreq(r.size, d=dlambda) * 2 * np.pi
 
     kk, zz = np.meshgrid(k, z)
@@ -36,31 +41,29 @@ def build_xi1d():
     xi_wwindow = np.fft.irfft(fid_pk_angstrom * window_squared_angstrom(k)) / dlambda
     xi_wwindow = np.fft.fftshift(xi_wwindow)
 
-    xi1d_interp = RGI((r, z), xi_wwindow.T, method='linear', bounds_error=False)
-    return xi1d_interp
+    return RGI((r, z), xi_wwindow.T, method='linear', bounds_error=False)
 
 
-def build_covariance(tracer, xi1d_interp):
+def build_inverse_covariance(tracer, xi1d_interp):
     z_ij = np.sqrt((1 + tracer.z[:, None]) * (1 + tracer.z[None, :])) - 1
-    indeces = np.arange(tracer.z.size)
-    wave_bin_size = 10**tracer.log_lambda[1] - 10**tracer.log_lambda[0]
+    wavelength = 10**tracer.log_lambda
 
-    delta_lambdas = np.abs(indeces[:, None] - indeces[None, :]) * wave_bin_size
+    delta_lambdas = wavelength[:, None] - wavelength[None, :]
     covariance = xi1d_interp((delta_lambdas, z_ij))
-
-    np.fill_diagonal(covariance, covariance.diagonal() + 1 / tracer.ivar)
+    covariance[np.diag_indices(tracer.z.size)] += 1 / tracer.ivar
 
     return np.linalg.inv(covariance)
 
 
 def get_xi_bins(tracer1, tracer2, angle):
-    rp = np.abs(np.subtract.outer(tracer1.dist_c, tracer2.dist_c) * np.cos(angle/2))
-    rt = np.add.outer(tracer1.dist_m, tracer2.dist_m) * np.sin(angle/2)
+    rp = np.abs(np.subtract.outer(tracer1.dist_c, tracer2.dist_c) * np.cos(angle / 2))
+    rt = np.add.outer(tracer1.dist_m, tracer2.dist_m) * np.sin(angle / 2)
 
     mask = (rp >= globals.rp_min) & (rp < globals.rp_max) & (rt < globals.rt_max)
-    bins_rp = ((rp - globals.rp_min) / (globals.rp_max - globals.rp_min) 
-               * globals.num_bins_rp).astype(int)
-    bins_rt = np.floor(rt / globals.rt_max * globals.num_bins_rt).astype(int)
+    bins_rp = (
+        (rp - globals.rp_min) / (globals.rp_max - globals.rp_min) * globals.num_bins_rp
+    ).astype(int)
+    bins_rt = (rt / globals.rt_max * globals.num_bins_rt).astype(int)
     bins = (bins_rt + globals.num_bins_rt * bins_rp).astype(int)
 
     return bins
